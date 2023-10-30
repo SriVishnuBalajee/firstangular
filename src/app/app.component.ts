bbe0560a31af8722972133b596537c1d8f2d3156
srivishnu.balaji <srivishnu.balaji@200411LTP2869>
2023-10-30 10:17:50 +0530

    commit 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:'<p>Welcome </p><br><p>Mr/Mrs&nbsp;{{name}}</p>',
+  // styles: ['b{color:red}','b:hover{color:blue}','div{color:red}']
+  
+ 
+  
+
 })
 export class AppComponent {
-  title = 'firstangular';
+  name=window.prompt("Code Required");
+ 
+  title = 'Srivishnu The MASS☠️ ';
+  ngOnInit(){
+    if(this.name!="Srivishnu"){
+      window.alert("Wrong Code");
+      window.location.reload();
+    }
+    else{
+      window.alert("Welcome Boss" )
+    }
+    
+  }
 }
